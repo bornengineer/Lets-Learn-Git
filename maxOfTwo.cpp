15b0c7727929c752b9f614900b92b6ed2686be87
Amaan Khan <amaanrkhan9@gmail.com>
2022-01-25 11:49:59 +0530

    initial commit

---
+++ maxOfTwo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+
+int main()
+{
+    int i, j;
+    cout << "Enter the numbers: " << endl;
+    cin >> i >> j;
+    cout << "The max of the numbers is:" << max(i, j);
+
+    return 0;
+}
